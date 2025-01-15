f8581b1e7c013752eb25a42ff32b9a65eaf53687
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2025-01-15 13:52:27 +0900

    複数指定可能にする

--- ahs/har_to_timing_csv.py
+++ ahs/har_to_timing_csv.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import re
 import sys
 from pathlib import Path
 from typing import Any
@@ -9,8 +10,9 @@ import pandas
 
 def create_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="HARファイルからtimingに関する情報をCSVとして出力します。")
-    parser.add_argument("har_file", type=Path)
+    parser.add_argument("har_file", type=Path, nargs="+", help="HARファイルのパス。")
     parser.add_argument("-o", "--output", type=Path, help="出力先。未指定ならば標準出力に出力します。")
+    parser.add_argument("--only_s3_path", action="store_true", help="AWS S3へアクセスしているリクエストのみを抽出します。")
     return parser
 
 
@@ -48,11 +50,17 @@ def minimize_entry(entry: dict[str, Any]) -> dict[str, Any]:
     return result
 
 
-def create_dataframe_from_har_object(data: dict[str, Any]) -> pandas.DataFrame:
+def match_entry(entry: dict[str, Any], is_s3_path) -> bool:
+    if is_s3_path:
+        url = entry["request"]["url"]
+        return re.search("https://.*amazonaws\\.com/", url) is not None
+
+
+def create_dataframe_from_har_object(data: dict[str, Any], *, is_s3_path: bool) -> pandas.DataFrame:
     """
     harファイルの内容をpandas.DataFrameに変換します。
     """
-    tmp_list = [minimize_entry(entry) for entry in data["log"]["entries"]]
+    tmp_list = [minimize_entry(entry) for entry in data["log"]["entries"] if match_entry(entry, is_s3_path)]
     df_har = pandas.json_normalize(tmp_list)
 
     columns = [
@@ -78,8 +86,18 @@ def main() -> None:
     parser = create_parser()
     args = parser.parse_args()
 
-    input_data = json.loads(args.har_file.read_text(encoding="utf-8"))
-    df_har = create_dataframe_from_har_object(input_data)
+    if len(args.har_file) == 1:
+        input_data = json.loads(args.har_file.read_text(encoding="utf-8"))
+        df_har = create_dataframe_from_har_object(input_data, is_s3_path=args.only_s3_path)
+    else:
+        df_har_list = []
+        for har_file in args.har_file:
+            input_data = json.loads(har_file.read_text(encoding="utf-8"))
+            df_sub_har = create_dataframe_from_har_object(input_data, is_s3_path=args.only_s3_path)
+            df_sub_har["har_file"] = str(har_file)
+            df_har_list.append(df_sub_har)
+        df_har = pandas.concat(df_har_list, ignore_index=True)
+
     if args.output is not None:
         output_file: Path = args.output
         output_file.parent.mkdir(exist_ok=True, parents=True)
